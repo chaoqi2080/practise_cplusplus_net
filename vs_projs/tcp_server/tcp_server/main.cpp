4ac2b2187455fbe6cfa40450b62b04cc85c4772e
chaoqi2081 <jcq_game@126.com>
2021-12-05 10:54:10 +0800

    增加 vs2019 工程配置，方便在 windows 上面做性能测试

---
+++ vs_projs/tcp_server/tcp_server/main.cpp
@@ -0,0 +1,24 @@
+﻿
+
+
+#include <stdio.h>
+#include <vector>
+
+#include "easy_tcp_server.hpp"
+
+
+int main() {
+    printf("[server start...]\n");
+    EasyTcpServer server;
+    server.listen(nullptr, 4567);
+    server.start();
+
+    //wait for new client connect
+    while (server.is_run()) {
+        server.on_run();
+    }
+
+    server.close();
+
+    return 0;
+}
